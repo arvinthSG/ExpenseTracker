61b085dfc979cf2dcfa190c36ec784a90eb720e6
DESKTOP-R6LFJ5R\arvin <arvinth.sg@gmail.com>
2018-11-10 13:41:28 -0700

    Commit: Added constants. Removed hardcoded data.

---
+++ app/src/main/java/io/sunhacks/com/expensetracker/Constants.java
@@ -0,0 +1,10 @@
+package io.sunhacks.com.expensetracker;
+
+public class Constants {
+    public static final String DISCOVER = "Discover";
+    public static final String CHASE = "Chase";
+    public static final String DISCOVER_PHONE_NO = "347268";
+    public static final String CHASE_PHONE_NO = "24273";
+    public static final String MIDFIRST = "MidFirst";
+    public static final String MIDFIRST_PHONE_NO = "20736";
+}
