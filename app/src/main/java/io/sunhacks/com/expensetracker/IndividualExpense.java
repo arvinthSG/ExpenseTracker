cbd6da7e42ee0f0cd3033741709cd9790622b6bb
Avinash <avinashcegian95@gmail.com>
2018-11-10 18:56:18 -0700

    Integration -- Pie chart

--- app/src/main/java/io/sunhacks/com/expensetracker/IndividualExpense.java
+++ app/src/main/java/io/sunhacks/com/expensetracker/IndividualExpense.java
@@ -5,15 +5,23 @@ import android.os.Bundle;
 
 import android.content.Context;
 import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import io.sunhacks.com.expensetracker.Adapter.IndividualExpenseAdapter;
@@ -32,17 +40,19 @@ public class IndividualExpense extends AppCompatActivity {
         setContentView(R.layout.activity_individual_expense);
 
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        TextView textTotalExpense = (TextView) findViewById(R.id.totalExpenseDollars);
+        textTotalExpense.setText("$ 400");
 
         individualExpenseAdapter = new IndividualExpenseAdapter(individualExpenseList);
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(mLayoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
         recyclerView.setAdapter(individualExpenseAdapter);
-
-        prepareMovieData();
+        prepareIndividualExpensesData();
 
     }
-    private void prepareMovieData() {
+    private void prepareIndividualExpensesData() {
         IndividualExpenseModel expense = new IndividualExpenseModel("Mad Max: Fury Road", 25);
         individualExpenseList.add(expense);
 
@@ -58,6 +68,18 @@ public class IndividualExpense extends AppCompatActivity {
         expense = new IndividualExpenseModel("The Martian", 10);
         individualExpenseList.add(expense);
 
+        Collections.sort(individualExpenseList, new SortbyProgress());
+
         individualExpenseAdapter.notifyDataSetChanged();
+    }
+}
+
+class SortbyProgress implements Comparator<IndividualExpenseModel>
+{
+    // Used for sorting in ascending order of
+    // roll number
+    public int compare(IndividualExpenseModel a, IndividualExpenseModel b)
+    {
+        return b.getProgress() - a.getProgress();
     }
 }
